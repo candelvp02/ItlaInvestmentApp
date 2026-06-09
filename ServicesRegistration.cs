048fc558058c167ce94c899969e90ab05ecbfed4
Candelaria Pereyra Villar <20242405@itla.edu.do>
2026-06-09 14:59:44 -0400

    Add files via upload

--- ServicesRegistration.cs
+++ ServicesRegistration.cs
@@ -1,23 +1,42 @@
-﻿using InvestmentApp.Core.Application.Interfaces;
-using InvestmentApp.Core.Application.Services;
+﻿using InvestmentApp.Core.Domain.Interfaces;
+using InvestmentApp.Infrastructure.Persistence.Contexts;
+using InvestmentApp.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace InvestmentApp.Core.Application
+namespace InvestmentApp.Infrastructure.Persistence
 {
     public static class ServicesRegistration
     {
         //Extension method - Decorator pattern
-        public static void AddApplicationLayerIoc(this IServiceCollection services)
-        {    
-            #region Services IOC
-            services.AddTransient<IAssetService, AssetService>();
-            services.AddTransient<IAssetTypeService, AssetTypeService>();
-            services.AddTransient<IUserService, UserService>();
-            services.AddTransient<IInvestmentPortfolioService, InvestmentPortfolioService>();
-            services.AddTransient<IAssetHistoryService, AssetHistoryService>();
-            services.AddTransient<IInvestmentAssetsService, InvestmentAssetsService>();
+        public static void AddPersistenceLayerIoc(this IServiceCollection services, IConfiguration config)
+        {
+            #region Contexts
+            if (config.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<InvestmentAppContext>(opt =>
+                                              opt.UseInMemoryDatabase("AppDb"));
+            }
+            else
+            {
+                var connectionString = config.GetConnectionString("DefaultConnection");
+                services.AddDbContext<InvestmentAppContext>(opt =>
+                opt.UseSqlServer(connectionString,
+                m=> m.MigrationsAssembly(typeof(InvestmentAppContext).Assembly.FullName))
+                , ServiceLifetime.Transient);
+            }
+            #endregion
+
+            #region Repositories IOC
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddTransient<IAssetRepository, AssetRepository>();
+            services.AddTransient<IAssetTypeRepository, AssetTypeRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IInvestmentPortfolioRepository, InvestmentPortfolioRepository>();
+            services.AddTransient<IInvestmentAssetRepository, InvestmentAssetRepository>();
+            services.AddTransient<IAssetHistoryRepository, AssetHistoryRepository>();
             #endregion
         }
-
     }
 }
